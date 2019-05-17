4b95c4d5762e8a329d4112f70b28856dfca51939
Lea Agterberg <lea.sara.agterberg@gmail.com>
2019-05-17 12:58:26 +0200

    Routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :cocktails, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
-    resources :ingredients, only: [ :index, :new, :create, :show, :edit, :update, :destroy]
-    resources :doses, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  root to: "cocktails#index"
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:create, :new]
   end
+  resources :doses, only: [:destroy]
 end
